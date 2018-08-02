0ec0c3badf566f76bbca1e1a7c56fb6b7dbc6893
BEKKIUCH Imad Eddine Ibrahim <bekkouche.imadeddine@gmail.com>
2018-08-02 10:53:13 +0100

    deployment

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
 
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/tensorflow-app'));
+app.use(express.static(__dirname + '/dist/tensorflowApp'));
 
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/tensorflow-app/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/tensorflowApp/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
