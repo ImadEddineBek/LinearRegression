796797f315a087faa27d6f52db7860c71568472b
BEKKIUCH Imad Eddine Ibrahim <bekkouche.imadeddine@gmail.com>
2018-08-02 10:19:49 +0100

    deployment

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/src'));
+// Replace the '/dist/<to_your_project_name>'
+app.use(express.static(__dirname + '/dist/LinearRegression'));
 
-app.get('/*', function(req,res) {
-
-  res.sendFile(path.join(__dirname+'/src/index.html'));
+app.get('*', function(req,res) {
+  // Replace the '/dist/<to_your_project_name>/index.html'
+  res.sendFile(path.join(__dirname + '/dist/LinearRegression/index.html'));
 });
-
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 8080);
